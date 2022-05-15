2ac0c3271a3f7d9d65f9b7d113891515b680cd4e
BB-choi <choibobae0809@gmail.com>
2022-05-15 20:36:09 +0900

    feat: 최소, 최대 카운트 초과시 alert 표시

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -12,13 +12,17 @@ const Counter = () => {
   const onClickDecreaseButton = () => {
     if (count > MIN_COUNT) {
       setCount(count - COUNT_PER_CLICK);
+      return;
     }
+    alert("더 이상 감소시킬 수 없습니다.");
   };
 
   const onClickIncreaseButton = () => {
     if (count < MAX_COUNT) {
       setCount(count + COUNT_PER_CLICK);
+      return;
     }
+    alert("더 이상 증가시킬 수 없습니다.");
   };
 
   const onClickResetButton = () => {
